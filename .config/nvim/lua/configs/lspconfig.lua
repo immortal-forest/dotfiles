4d3518d3343d4b11f498dc38b1b03ebffaac9299
immortal-forest <ali.salman.sa.01@gmail.com>
2024-07-30 19:54:48 +0545

    fix: use default pyright settings

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -3,21 +3,20 @@ local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
 
-lspconfig.pyright.setup({
+lspconfig.pyright.setup {
   on_attach = on_attach,
   capabilities = capabilities,
-  filetypes = {"python"},
-  settings = {
-    pyright = {
-      -- use ruff instead
-      disableOrganizeImports = true,
-    },
-    python = {
-      analysis = {
-        -- Ignore all files for analysis to exclusively use Ruff for linting
-        ignore = { '*' },
-      },
-    },
-  },
-})
-
+  filetypes = { "python" },
+  -- settings = {
+  -- pyright = {
+  -- use ruff instead
+  -- disableOrganizeImports = true,
+  -- },
+  -- python = {
+  -- analysis = {
+  -- Ignore all files for analysis to exclusively use Ruff for linting
+  -- ignore = { "*" },
+  -- },
+  -- },
+  -- },
+}
