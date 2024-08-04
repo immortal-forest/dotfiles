744d43f8f88f5a5c457efbcc188254ad523039aa
immortal-forest <ali.salman.sa.01@gmail.com>
2024-08-04 23:14:55 +0545

    refactor: show nvdash on startup

--- .config/nvim/lua/chadrc.lua
+++ .config/nvim/lua/chadrc.lua
@@ -1,20 +1,24 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v2.5/lua/nvconfig.lua
 
 ---@type ChadrcConfig
 local M = {}
 
 M.ui = {
-	theme = "catppuccin",
+  theme = "catppuccin",
   statusline = {
     theme = "minimal",
     separator_style = "round",
   },
 
-	hl_override = {
-		Comment = { italic = true },
-		["@comment"] = { italic = true },
-	},
+  hl_override = {
+    Comment = { italic = true },
+    ["@comment"] = { italic = true },
+  },
+
+  nvdash = {
+    load_on_startup = true,
+  },
 }
 
 return M
