f2c52d2046b7db86c3485873255dde334ec34ab3
immortal-forest <ali.salman.sa.01@gmail.com>
2024-09-04 20:43:27 +0545

    feat: add others and few more options

--- .config/scripts/cht.sh
+++ .config/scripts/cht.sh
@@ -1,15 +1,18 @@
 #! /usr/bin/sh
 
-languages=`echo "python kotlin" | tr ' ' '\n'`
+languages=$(echo "bash kotlin lua python python3 java c js" | tr ' ' '\n' | sort)
+other=$(echo "git flask django" | tr ' ' '\n' | sort)
 core="$(compgen -c | grep -v '^_' | tr ' ' '\n')"
 
-opt=`echo "language core" | tr ' ' '\n'`
-opt_selected=`printf "$opt" | fzf`
+opt=$(echo "language core other" | tr ' ' '\n' | sort)
+opt_selected=$(printf "$opt" | fzf)
 
 if [[ $opt_selected == "language" ]]; then
-  selected=`printf "$languages" | fzf`
+  selected=$(printf "$languages" | fzf)
 elif [[ $opt_selected == "core" ]]; then
-  selected=`printf "$core" | fzf`
+  selected=$(printf "$core" | fzf)
+elif [[ $opt_selected == "other" ]]; then
+  selected=$(printf "$other" | fzf)
 else
   exit 1
 fi
@@ -19,11 +22,13 @@ if [[ $selected == "" ]]; then
 fi
 
 read -p "Enter Query: " query
-query=`echo $query | tr ' ' '+'`
+query=$(echo $query | tr ' ' '+')
 clear
 
 if [[ $opt_selected == "language" ]]; then
   echo "curl cht.sh/$selected/$query" && curl -s cht.sh/$selected/$query | less -R
+elif [[ $opt_selected == "other" ]]; then
+  curl -s cht.sh/$selected-$query | less -R
 else
   curl -s cht.sh/$selected~$query | less -R
 fi
