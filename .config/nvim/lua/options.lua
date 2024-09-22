4d5a4499c7baad3864a892437080887b27b4796f
immortal-forest <ali.salman.sa.01@gmail.com>
2024-09-22 22:41:42 +0545

    refactor: enable cursorline and relative line numbers

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,6 +1,8 @@
 require "nvchad.options"
 
--- add yours here!
+local o = vim.o
+o.cursorlineopt = "both" -- to enable cursorline!
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+local opt = vim.opt
+
+opt.relativenumber = true
