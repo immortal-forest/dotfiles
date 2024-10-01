2fd27e8de2192df98f7217e459a4cedbd5873b23
immortal-forest <ali.salman.sa.01@gmail.com>
2024-10-01 10:21:43 +0545

    refactor: use ruff for organize imports instead of isort

--- .config/nvim/lua/configs/conform.lua
+++ .config/nvim/lua/configs/conform.lua
@@ -4,14 +4,8 @@ local options = {
   formatters_by_ft = {
     lua = { "stylua" },
     sh = { "shfmt" },
-    python = function(bufnr)
-      if require("conform").get_formatter_info("ruff_format", bufnr).available then
-        return { "ruff_format" }
-      else
-        return { "isort", "black" }
-      end
-    end,
-    c = { "uncrustify" },
+    python = { "ruff_organize_imports", "black" },
+    c = { "astyle", "uncrustify" },
     json = { "fixjson" },
     jsonc = { "fixjson" },
     css = { "prettier" },
