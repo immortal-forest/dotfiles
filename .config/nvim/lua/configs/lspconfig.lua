bd93d78bf77aa5c95cca2fe22f8bdd426ae79dae
immortal-forest <ali.salman.sa.01@gmail.com>
2024-10-01 10:38:12 +0545

    refactor: update lsp configs

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -1,22 +1,35 @@
-local on_attach = require("nvchad.configs.lspconfig").on_attach
-local capabilities = require("nvchad.configs.lspconfig").capabilities
+require("nvchad.configs.lspconfig").defaults()
 
 local lspconfig = require "lspconfig"
 
+local servers = {}
+local nvlsp = require "nvchad.configs.lspconfig"
+
+-- lsps with default config
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = nvlsp.on_attach,
+    on_init = nvlsp.on_init,
+    capabilities = nvlsp.capabilities,
+  }
+end
+
+-- single server setup
 lspconfig.pyright.setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
+  on_attach = nvlsp.on_attach,
+  on_init = nvlsp.on_init,
+  capabilities = nvlsp.capabilities,
   filetypes = { "python" },
-  -- settings = {
-  -- pyright = {
-  -- use ruff instead
-  -- disableOrganizeImports = true,
-  -- },
-  -- python = {
-  -- analysis = {
-  -- Ignore all files for analysis to exclusively use Ruff for linting
-  -- ignore = { "*" },
-  -- },
-  -- },
-  -- },
+  settings = {
+    pyright = {
+      -- use ruff instead
+      disableOrganizeImports = true,
+    },
+    python = {
+      analysis = {
+        -- Ignore all files for analysis to exclusively use Ruff for linting
+        ignore = { "*" },
+      },
+    },
+  },
 }
