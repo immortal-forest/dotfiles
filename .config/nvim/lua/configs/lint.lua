817a3123371555ce5600b3446975db219a2873d4
immortal-forest <ali.salman.sa.01@gmail.com>
2024-10-04 22:46:00 +0545

    feat: add linting for lua and bash scripts

--- .config/nvim/lua/configs/lint.lua
+++ .config/nvim/lua/configs/lint.lua
@@ -1,7 +1,9 @@
 local lint = require "lint"
 
 lint.linters_by_ft = {
+  lua = { "luacheck" },
   python = { "ruff" },
+  sh = { "bash" },
   javascript = { "biomejs" },
   typescript = { "biomejs" },
 }
