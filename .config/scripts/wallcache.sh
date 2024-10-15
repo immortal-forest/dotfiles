c744aff2c5c4a917f59569ce10aa5dc37d2e5cab
immortal-forest <ali.salman.sa.01@gmail.com>
2024-10-15 18:15:28 +0545

    fix: no blur image

--- .config/scripts/wallcache.sh
+++ .config/scripts/wallcache.sh
@@ -10,11 +10,11 @@ else
   cp "$1" "$thmbDir/wall.set.png"
 fi
 
+convert "$thmbDir/wall.set.png"[0] -strip -scale 10% -blur 0x3 -resize 100% "$thmbDir/wall.blur" &
+
 convert -strip -resize 1000 -gravity center -extent 1000 -quality 90 "$thmbDir/wall.set.png"[0] "$thmbDir/wall.thmb" &
 
 convert -strip "$thmbDir/wall.set.png"[0] -thumbnail 500x500^ -gravity center -extent 500x500 "$thmbDir/wall.sqre"
-
-convert -strip -scale 10% -blur 0x3 -resize 100% "$thmbDir/wall.set.png"[0] "$thmbDir/wall.blur" &
 
 convert "$thmbDir/wall.sqre" \( -size 500x500 xc:white -fill "rgba(0,0,0,0.7)" -draw "polygon 400,500 500,500 500,0 450,0" -fill black -draw "polygon 500,500 500,0 450,500" \) -alpha Off -compose CopyOpacity -composite "$thmbDir/wall.png" && mv "$thmbDir/wall.png" "$thmbDir/wall.quad" &
 
