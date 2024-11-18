70d8e015e03d5bc11be3f392801d46b9e0de91d7
immortal-forest <ali.salman.sa.01@gmail.com>
2024-11-18 17:12:33 +0545

    fix: brightness value

--- .config/scripts/wallgen.sh
+++ .config/scripts/wallgen.sh
@@ -4,7 +4,7 @@ wall_path=$1
 theme="dark"
 
 fxb=$(magick "$wall_path" -colorspace gray -resize 1280x720 -format "%[fx:mean]" info:)
-if awk -v fxb="${fxb}" 'BEGIN {exit !(fxb < 0.72)}'; then
+if awk -v fxb="${fxb}" 'BEGIN {exit !(fxb < 0.71)}'; then
   # dark
   wal -i "$wall_path" -n -e &>/dev/null
 else
