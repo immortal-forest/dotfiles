a93c6b707a81de6882aa8df41c1b5083b5b8b838
immortal-forest <ali.salman.sa.01@gmail.com>
2024-11-26 17:44:51 +0545

    get theme from hyprpanel

--- .config/scripts/wallgen.sh
+++ .config/scripts/wallgen.sh
@@ -2,13 +2,13 @@
 
 wall_path=$1
 
-fxb=$(magick "$wall_path" -colorspace gray -resize 1280x720 -format "%[fx:mean]" info:)
-if awk -v fxb="${fxb}" 'BEGIN {exit !(fxb < 0.71)}'; then
+theme=$(sed -n 's/.*"theme.matugen_settings.mode": "\(.*\)".*/\1/p' .cache/ags/hyprpanel/options.json)
+if [[ "$theme" == "dark" ]]; then
   # dark
-  theme="dark"
+  wal -i "$wall_path" -n -e &>/dev/null
 else
   # light
-  theme="light"
+  wal -i "$wall_path" -l -n -e &>/dev/null
 fi
 
 wallupdate.sh $2 $theme  &>/dev/null
