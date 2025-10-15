f875af34aab55cb9b5aeaf385f5a34324781bcb8
immortal-forest <ali.salman.sa.01@gmail.com>
2025-10-15 11:26:08 +0800

    feat(nvim): add formatter enable/disable and nvdash

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -20,3 +20,33 @@ vim.api.nvim_create_autocmd("LspAttach", {
   end,
   desc = "LSP: Disable hover capability from Ruff",
 })
+
+vim.api.nvim_create_autocmd("BufDelete", {
+  callback = function()
+    local bufs = vim.t.bufs
+    if #bufs == 1 and vim.api.nvim_buf_get_name(bufs[1]) == "" then
+      vim.cmd "Nvdash"
+    end
+  end,
+})
+
+-- Enable/Disable formatter
+vim.api.nvim_create_user_command("FormatDisable", function(args)
+  if args.bang then
+    -- :FormatDisable! disables autoformat for this buffer only
+    vim.b.disable_autoformat = true
+  else
+    -- :FormatDisable disables autoformat globally
+    vim.g.disable_autoformat = true
+  end
+end, {
+  desc = "Disable autoformat-on-save",
+  bang = true, -- allows the ! variant
+})
+
+vim.api.nvim_create_user_command("FormatEnable", function()
+  vim.b.disable_autoformat = false
+  vim.g.disable_autoformat = false
+end, {
+  desc = "Re-enable autoformat-on-save",
+})
