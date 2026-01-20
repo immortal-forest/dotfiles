0ef2081a126ad255cc1b67bc9b6909806faabcdd
immortal-forest <ali.salman.sa.01@gmail.com>
2026-01-20 10:25:36 +0800

    refactor(nvim): move r.nvim config under configs dir

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -24,41 +24,7 @@ return {
     "R-nvim/R.nvim",
     lazy = false,
     config = function()
-      -- Create a table with the options to be passed to setup()
-      ---@type RConfigUserOpts
-      local opts = {
-        hook = {
-          on_filetype = function()
-            vim.keymap.set("n", "<Enter>", "<Plug>RDSendLine", { buffer = 0 })
-            vim.keymap.set("v", "<Enter>", "<Plug>RSendSelection", { buffer = 0 })
-          end,
-        },
-        R_args = { "--quiet", "--no-save" },
-        min_editor_width = 72,
-        rconsole_width = 78,
-        objbr_mappings = { -- Object browser keymap
-          c = "class", -- Call R functions
-          ["<localleader>gg"] = "head({object}, n = 15)", -- Use {object} notation to write arbitrary R code.
-          v = function()
-            -- Run lua functions
-            require("r.browser").toggle_view()
-          end,
-        },
-        disable_cmds = {
-          "RClearConsole",
-          "RCustomStart",
-          "RSPlot",
-          "RSaveClose",
-        },
-      }
-      -- Check if the environment variable "R_AUTO_START" exists.
-      -- If using fish shell, you could put in your config.fish:
-      -- alias r "R_AUTO_START=true nvim"
-      if vim.env.R_AUTO_START == "true" then
-        opts.auto_start = "on startup"
-        opts.objbr_auto_start = true
-      end
-      require("r").setup(opts)
+      require "configs.r"
     end,
   },
 
